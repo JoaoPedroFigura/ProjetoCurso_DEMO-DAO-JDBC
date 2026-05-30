6fad557d0f24e0d75d63592f9af8396c1baa1c42
JoaoPedroFigura <joaopedrofiguraburkot@gmail.com>
2026-05-30 01:16:40 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import entities.Department;
+
+public class Program {
+    static void main() {
+       Department obj = new Department(1, "Books");
+        System.out.println(obj);
+    }
+
+
+
+}
