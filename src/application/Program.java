6150365a1059fe785b2babe190ca852aab56f207
JoaoPedroFigura <joaopedrofiguraburkot@gmail.com>
2026-05-31 00:16:08 -0300

    FindByDeparment Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ import entities.Department;
 import entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
     static void main() {
@@ -17,6 +18,13 @@ public class Program {
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
+
+        System.out.println("=== TEST 2: seler findByDepartment ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list){
+            System.out.println(obj);
+        }
 
 
     }
